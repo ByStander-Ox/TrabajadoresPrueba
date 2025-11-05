bef26b17acf70ad22167bc376031c3af03f63c48
Sergio <SergioXD753@hotmail.com>
2025-11-05 13:00:33 -0500

    Vistas de create, update, delete

---
+++ TrabajadoresPrueba/Views/Trabajador/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrabajadoresPrueba.Views.Trabajador
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
