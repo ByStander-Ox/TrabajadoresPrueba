fa3f0a28ab9bf2b03538ff2f1227e8e6cabab53a
Sergio <SergioXD753@hotmail.com>
2025-11-05 11:34:40 -0500

    Configuración del contexto de base de datos (DbContext)

---
+++ TrabajadoresPrueba/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrabajadoresPrueba.Models;
+
+namespace TrabajadoresPrueba.Data
+{
+   
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet -> tabla de la base de datos
+        public DbSet<Trabajador> Trabajadores { get; set; }
+
+        // Configuración adicional
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
